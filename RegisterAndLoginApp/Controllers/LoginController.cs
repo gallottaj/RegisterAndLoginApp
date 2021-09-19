c25422f766cf23a08e52ad2598e9cf95c8e4bb7a
Joey Gallotta <joeygallotta@Joeys-MacBook-Pro.local>
2021-09-19 12:33:02 -0700

    add security service for known users

--- RegisterAndLoginApp/Controllers/LoginController.cs
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RegisterAndLoginApp.Models;
+using RegisterAndLoginApp.Services;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,14 +21,15 @@ namespace RegisterAndLoginApp.Controllers
         //accept a user model and send it as a parameter to the next view
         public IActionResult ProcessLogin(UserModel userModel)
         {
-            if (userModel.UserName == "BillGates" && userModel.Password == "bigbucks")
+            SecurityService securityService = new SecurityService();
+            if (securityService.IsValid(userModel))
             {
                 return View("LoginSuccess", userModel);
 
             }
             else
             {
-                return View("LoginSuccess", userModel);
+                return View("LoginFailure", userModel);
 
             }
         }
---
+++ RegisterAndLoginApp/Services/SecurityService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RegisterAndLoginApp.Models;
+
+namespace RegisterAndLoginApp.Services
+    //used to authenticate a user
+{
+    public class SecurityService
+    {
+        List<UserModel> knowUsers = new List<UserModel>();
+
+        public SecurityService()
+        {
+            knowUsers.Add(new UserModel { Id = 0, UserName = "BillGates", Password = "bigbucks" });
+            knowUsers.Add(new UserModel { Id = 1, UserName = "MarieCurie", Password = "radioactive" });
+            knowUsers.Add(new UserModel { Id = 2, UserName = "WatsonCrick", Password = "dna" });
+            knowUsers.Add(new UserModel { Id = 3, UserName = "AlexanderFlemming", Password = "peniciliin" });
+        }
+        //method to find out if it is a valid user login
+        public bool IsValid(UserModel user)
+        {
+            //return true if the user is found in the list
+            return knowUsers.Any(x => x.UserName == user.UserName && x.Password == user.Password);
+        }
+    }
+ }
