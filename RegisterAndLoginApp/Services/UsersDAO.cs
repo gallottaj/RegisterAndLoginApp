5e0ce9ce70d254a88f594ce2af70895a530a183b
Joey <gallo@DESKTOP-BO1GFI1>
2021-09-19 15:17:41 -0700

    add sql db connection in UsersDAO

---
+++ RegisterAndLoginApp/Services/UsersDAO.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using RegisterAndLoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterAndLoginApp.Services
+{
+    public class UsersDAO
+    {
+        string connectionString = @"Data Source = (localdb)\ProjectsV13;Initial Catalog = Test; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public bool FindUserByNameAndPassowrd(UserModel user)
+        {
+            //success is set to false by default
+            bool success = false;
+            string sqlStatement = "SELECT * FROM dbo.Users WHERE username = @username AND password = @password ";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.Add("@username", System.Data.SqlDbType.VarChar, 40).Value = user.UserName;
+                command.Parameters.Add("@password", System.Data.SqlDbType.VarChar, 40).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    //if any rows are found
+                    if (reader.HasRows);
+                    {
+                        success = true;
+                    }
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            //return success as a true or false value
+            return success;
+        }
+    }
+}
